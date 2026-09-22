27ed5f50c1611bbf3bd6c97e71e0f00866780e71
Tilman Porschuetz <mandymozart@viennastruggle.com>
2026-09-22 17:20:10 +0200

    Fix console log formatting in addPaymentNotice

--- src/Resources/app/storefront/dist/storefront/js/nadeos/nadeos.js
+++ src/Resources/app/storefront/dist/storefront/js/nadeos/nadeos.js
@@ -1,2 +1,3 @@
 function addPaymentNotice() {
-console.log('Theme: nadeos scripts loaded. Inject patches here if you like.')
+  console.log('Theme: nadeos scripts loaded. Inject patches here if you like.')
+}
